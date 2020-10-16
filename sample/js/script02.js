181f5e6637a1965953e3c33079d28e1d40a79dbf
GF13-017 <68629581+GF13-017@users.noreply.github.com>
2020-10-16 13:46:26 +0900

    ぴ

--- sample/js/script02.js
+++ sample/js/script02.js
@@ -19,13 +19,14 @@ $(function(){
   let txt3 = "眠いですね";
   let num1 = 100;
   let num2 = 50;
+  let num3 = 50;
 
   $("#button04").on("click",function(){
     $("#content").text(txt1);
   })
 
   $("#button05").on("click",function(){
-    $("#content").text(num1 + num2);
+    $("#content").text(num1 + num2+num3);
   })
   
 
